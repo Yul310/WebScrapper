15762bc8a9bb68009c943ecaf2d8176906be3247
Yul310 <hsy3102@gmail.com>
2023-03-08 00:31:34 -0500

    main.py updated

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from requests import get
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "python"
 
-get(f"{base_url}{search_term}")
+response = get(f"{base_url}{search_term}")
+
+print(response)
