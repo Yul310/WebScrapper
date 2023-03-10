a06b448589b7dd7505719f4adee1dedf79b61d37
Yul310 <hsy3102@gmail.com>
2023-03-09 23:44:25 -0500

    beautiful soup updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "python"
@@ -8,6 +9,7 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request the website")
 else:
-    print(response.text)
+    soup = BeautifulSoup(response.text,"html.parser")
+    print(soup.find_all('section',class_="jobs"))
 
-print(response.text)
+# print(response.text)
