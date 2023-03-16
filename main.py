7a16621fe0d26408a759d9a54530ecd93f7bde38
Yul310 <hsy3102@gmail.com>
2023-03-16 00:05:48 -0400

    indeed try and 403 error

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
 
-jobs = extract_wwr_jobs('python')
-print(jobs)
+# base_url = 'https://www.indeed.com/jobs?q='
+# search_term = "python"
+# end_url = "&l=&vjk=d598cdf1dbd6f494"
+test_url = "https://www.indeed.com/jobs?q=python&l=&vjk=d598cdf1dbd6f494"
+# response = get(f"{base_url}{search_term}{end_url}")
+response = get(f"{test_url}")
+
+if response.status_code != 200:
+    print(response)
+    print("Can't request page")
+else:
+    print(response.text)
+
+# jobs = extract_wwr_jobs('python')
+# print(jobs)
 
