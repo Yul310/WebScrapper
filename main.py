6cfa39bf9f3ab9743fd7fb4ebda63557c8c91941
Yul310 <hsy3102@gmail.com>
2023-03-17 23:48:38 -0400

    403 error debugging using selenium

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from requests import get
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+driver = webdriver.Chrome()
+driver.get('https://kr.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=89395b6ac5014113')
+
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
 
 # base_url = 'https://www.indeed.com/jobs?q='
 # search_term = "python"
 # end_url = "&l=&vjk=d598cdf1dbd6f494"
-test_url = "https://www.indeed.com/jobs?q=python&l=&vjk=d598cdf1dbd6f494"
+# test_url = "https://www.indeed.com/jobs?q=python&l=&vjk=d598cdf1dbd6f494"
 # response = get(f"{base_url}{search_term}{end_url}")
 response = get(f"{test_url}")
 
