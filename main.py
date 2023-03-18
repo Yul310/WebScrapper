470cc6628da892e47307c08c42ab96574f9ecf7e
Yul310 <hsy3102@gmail.com>
2023-03-18 00:06:19 -0400

    403 error debugging using selenium try3, selenium working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from requests import get
+
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
 
 driver = webdriver.Chrome()
 # driver.get('https://kr.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=89395b6ac5014113')
@@ -13,13 +13,19 @@ from extractors.wwr import extract_wwr_jobs
 # end_url = "&l=&vjk=d598cdf1dbd6f494"
 # test_url = "https://www.indeed.com/jobs?q=python&l=&vjk=d598cdf1dbd6f494"
 # response = get(f"{base_url}{search_term}{end_url}")
-response = driver.get('https://www.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=7537cb6fcb8ab5c2')
+# browser = driver.get('https://www.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=7537cb6fcb8ab5c2')
+browser = webdriver.Chrome()
+browser.get('https://www.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=7537cb6fcb8ab5c2')
 
-if response.status_code != 200:
-    print(response)
-    print("Can't request page")
-else:
-    print(response.text)
+
+print(browser)
+print(browser.page_source)
+
+# if response.status_code != 200:
+#     print(response)
+#     print("Can't request page")
+# else:
+#     print(response.text)
 
 # jobs = extract_wwr_jobs('python')
 # print(jobs)
