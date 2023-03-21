f86d23a5f5a2e017272930a8ac6a13c8df7671c8
Yul310 <hsy3102@gmail.com>
2023-03-21 00:04:23 -0400

    data selection and extraction updated

--- main.py
+++ main.py
@@ -13,14 +13,29 @@ soup=BeautifulSoup(driver.page_source, 'html.parser')
 job_list = soup.find('ul', class_='jobsearch-ResultsList')
 jobs = job_list.find_all('li', recursive=False)
 
+results = []
+
 for job in jobs:
     zone = job.find('div', class_='mosaic-zone')
     if zone == None:
         anchor = job.select_one("h2 a")
         title = anchor['aria-label']
         link = anchor['href']
-        print(title, link)
-        print("///////\n")
+        company = job.find("span", class_="companyName")
+        location = job.find("div", class_="companyLocation")
+        job_data = {
+            'company':company.string,
+            'position': title,
+            'location': location.string,
+            'link':f"https://indeed.com{link}"
+
+        }
+        results.append(job_data)
+
+for result in results:
+    print(result)
+        # print(title, link)
+        # print("///////\n")
 
 
         # h2 = job.find("h2", class_="jobTitle")
