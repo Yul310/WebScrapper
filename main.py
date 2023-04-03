90eb47aeb2ed9b63e428139e78ad9d37f2f1fac1
Yul310 <hsy3102@gmail.com>
2023-04-03 00:41:00 -0400

    flask test 01

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ from flask import Flask
 
 app = Flask("JobScrapper")
 
+@app.route("/")
+def home():
+    return 'hey there!'
+
+@app.route("/hello")
+def hello():
+    return 'hello you!'
+
 app.run("0.0.0.0")
 
 
